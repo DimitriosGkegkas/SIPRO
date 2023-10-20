549383dd019c8500e8dfbfe01e42de9e9d735eef
Dimitrios Gkegkas <dgkegkas2023@ec-nantes.local>
2023-10-20 10:55:05 +0200

    Fix PID controller

--- Labs/lab2/decodeur.m
+++ Labs/lab2/decodeur.m
@@ -12,6 +12,10 @@ kmax = round(npt*fmax/fs) + 1;
 
 ytf = [zeros(kmin, 1, 'like', ytf_compresses); ytf_compresses; zeros(kmax, 1, 'like', ytf_compresses)];
 
+% Add the mirror image of the frequences cause the signal has to be real
+% valued in time domain
+ytf = [ytf, fliplr(ytf)];
+
 
 y = real(ifft(ytf, npt));
 
